74b4d637f3e657cbf38ce2f1f45df646486fe738
FranceMeli <f_melighetti@libero.it>
2021-05-11 23:28:47 +0200

    new fix

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>VC 2008</h1>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <!--  <b-navbar-brand href="#">VC 2008</b-navbar-brand>-->
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <!--      <b-nav-item href="#">mini challenge 1</b-nav-item>-->
+          <b-nav-item href="#">Mini Challenge 2</b-nav-item>
+          <!--      <b-nav-item href="#">Mini Challenge 3</b-nav-item>-->
+          <!--      <b-nav-item href="#">Mini Challenge 4</b-nav-item>-->
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <MC2Vehicles></MC2Vehicles>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MC2Vehicles from './components/MC2Vehicles.vue'
+const d3 = require("d3");
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MC2Vehicles
+  },
+  mounted() {
+
+    let pollo = ['Cabbage', 'Turnip', 'Radish', 'Carrot']
+    let a = { "Mario":"7", "Gianni":"4", "Monica":"4" };
+    let classi = []
+    classi[0]= ["Aldo","Giovanni", "Giacomo"]
+    classi[1]= ["m","g","h"];
+
+    // eslint-disable-next-line no-undef
+    d3.tsv("b.txt").then(function(data) {
+      console.log(data); // [{"Hello": "world"}, …]
+      console.log(pollo)
+      console.log(a)
+      console.log(classi)
+    })
+
   }
 }
 </script>
---
+++ src/components/MC2Vehicles.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    <h1>MC2Vehicles</h1>
+  </div>
+</template>
+
+<script>
+const d3 = require("d3");
+export default {
+  name: 'MC2Vehicles',
+  mounted() {
+
+    let data = [42, 4, 8, 15, 16, 23];
+    d3.select("div")  // We need to get an element
+        .append("div").classed("chart", true) // We add the chart graph container element (The div .chart element)
+        .selectAll("div") // We select all div but we will get a null selection because their is no div element
+        .data(data) // We join the empty selection with the data
+        .enter() // Their is no data, therefore only the enter set of the data join contains nodes without element
+        .append("div") // We append for each data a div element with the below style
+        .style("width", function(d) { return d * 10 + "px"; })
+        .text(function(d) { return d; });
+    /*d3.tsv('b.txt')
+        .then((rows) => {
+          const ids = rows
+              .filter((row) => {
+                const entries = d3.values(row);
+                return (entries[0].length > 0); // ignore rows with invalid id (not numeric)
+              })
+              .map((row) => {
+                const entries = d3.values(row);
+                return {
+                  id: +entries[0],
+                  person: entries[1],
+                };
+              });
+          this.persons = ids;
+
+        });
+    console.log(p)*/
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
