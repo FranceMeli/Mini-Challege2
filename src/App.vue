e07fdab4233eff030858e03c506782c2ecf4ec9a
FranceMeli <f_melighetti@libero.it>
2021-05-17 00:44:58 +0200

    fix map

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
     <h1>VC 2008</h1>
+    <h2>Mini-Challenge 2</h2>
     <b-navbar toggleable="lg" type="dark" variant="info">
       <!--  <b-navbar-brand href="#">VC 2008</b-navbar-brand>-->
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <!--      <b-nav-item href="#">mini challenge 1</b-nav-item>-->
-          <b-nav-item href="#">Mini Challenge 2</b-nav-item>
-          <!--      <b-nav-item href="#">Mini Challenge 3</b-nav-item>-->
-          <!--      <b-nav-item href="#">Mini Challenge 4</b-nav-item>-->
+          <b-nav-item href="#">General Results</b-nav-item>
+          <b-nav-item href="#">Vehicles</b-nav-item>
+          <b-nav-item href="#">Credit-Cards</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
-    </b-navbar>
-    <MC2Vehicles></MC2Vehicles>
+   </b-navbar>
+  <!--  <MC2Vehicles></MC2Vehicles>-->
     <MC2Card></MC2Card>
   </div>
 </template>
 
 <script>
-import MC2Vehicles from './components/MC2Vehicles.vue'
+//import MC2Vehicles from './components/MC2Vehicles.vue'
 import MC2Card from './components/MC2Card.vue'
 //const d3 = require("d3");
 export default {
   name: 'App',
   components: {
     MC2Card,
-    MC2Vehicles
+ //   MC2Vehicles
   },
   mounted() {
 
@@ -41,7 +41,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  /*color: #2c3e50;*/
   margin-top: 60px;
 }
 </style>
--- src/components/MC2Card.vue
+++ src/components/MC2Card.vue
@@ -1,72 +1,138 @@
 <template>
   <div class="mc2Card">
     <h1>MC2Card</h1>
-    <div id="Map"></div>
+    <b-row>
+      <b-col>
+        <div id="Map"></div>
+        <div id="sliderOre">
+          <input id="timeOre" type="range" min="0" max="8640" value="0" step="1"/><br>
+          <span id="rangeOre">00.00.00</span>
+        </div>
+        <div id="sliderGiorni">
+          <input id="timeGiorni" type="range" min="0" max="14" value="0" step="1"/><br>
+          <span id="rangeGiorni">01/06/2014</span>
+        </div>
+      </b-col>
+      <b-col>
+        <h2>Persons</h2>
+        <b-list-group class="personList">
+          <b-list-group-item v-for="p in persons" :key="p.id"
+                             :variant="p.selected ? 'warning': ''"
+                             class="d-flex justify-content-between align-items-center">
+            <b-badge variant="primary" pill>{{p.id}}</b-badge>
+            {{p.person}}
+          </b-list-group-item>
+        </b-list-group>
+      </b-col>
+    </b-row>
+
   </div>
 </template>
 
 <script>
  const d3 = require("d3");
- const topojson = require("topojson")
+ //const topojson = require("topojson")
 //const d3 = require("https://d3js.org/d3.v5.min.js")
-// const topojson = require("topojson")
+const topojson = require("topojson")
 export default {
   name: 'MC2Card',
-  mounted() {
-
-
-    var width = 600,
-        height = 300;
-
-    var svg = d3.select('#Map')
-        .append("svg")
-        .attr('width', width)
-        .attr('height', height);
-
-
-
-    d3.json("https://raw.githubusercontent.com/FranceMeli/Mini-Challege2/master/static/Geospatial/AbilaT.json").then(function(mapData) {
-      var features = topojson.feature(mapData, mapData.objects.Abila1);
-   //   var features = mapData.features;
-      console.log(features)
-      /*var projection = d3.geoIdentity()
-          .fitExtent([[50,50],[600-50,300-50]], features)*/
-      let geop = d3.geoPath(null)
-      console.log(geop)
-      svg.append("g")
-          .attr("class", "Abila1")
-          .selectAll("path")
-          .data(mapData.features)
-          .enter().append("path")
-          .attr("fill", "gray")
-          .attr("d", geop);
-    });
-
-    //Create SVG element
-   /* var svg = d3.select("#Map").append("svg").attr({width:w, height: h});
-
-    //Load in GeoJSON data
-    d3.json("https://raw.githubusercontent.com/FranceMeli/Mini-Challege2/master/static/Geospatial/Abila1.json", function(json) {
-      console.log("A")
-      //Bind data and create one path per GeoJSON feature
-      svg.selectAll("path")
-          .data(json.features)
+  data() {
+    return {
+      persons: [],
+      timeInterval: 10,
+    };
+  },
+  mounted: function () {
+    let A;
+    var inputGiorno= null;
+ //   var inputOra = null;
+    var Giorni = ["01/06/2014", "01/08/2014", "01/09/2014"];
+   // var Ore = [""]
+    function createMap(features, cities) {
+      var width = 900;
+      var height = 600;
+       /* var bounds = d3.geoBounds(features);
+        var centerX = d3.sum(bounds, function(d) {return d[0];}) / 2;
+        var centerY = d3.sum(bounds, function(d) {return d[1];}) / 2;*/
+         var projection = d3.geoMercator()
+            // .scale(460000)
+             .center([24.8669975, 36.0699665])
+             .fitSize([width - 30, height - 30], features);
+      var geoPath = d3.geoPath().projection(projection);
+      var svg = d3.select('#Map')
+          .append("svg")
+          // .attr('id', "mappa")
+          .attr('width', width)
+          .attr('height', height);
+      svg.selectAll("circle")
+          .data(cities.map(function(d) { return d.value; }))
+          .enter()
+          .append("circle")
+          .attr("class", "move")
+          .attr("r", 3)
+          .attr("cx", function(d) {return projection([d.long,d.lat])[0]})
+          .attr("cy", function(d) {return projection([d.long,d.lat])[1]})
+          .attr("fill", initialDate)
+      svg.selectAll("path").data(features.features)
           .enter()
           .append("path")
-          .attr("d", path)
-          .attr("fill","#666666");
+          .attr("d", geoPath)
+    }
+    let features;
+    const dataset = [];
+    const riprova=[];
+    d3.json("https://raw.githubusercontent.com/FranceMeli/Mini-Challege2/master/static/Geospatial/AbilaT.json").then(function (mapData) {
+      features = topojson.feature(mapData, mapData.objects.Abila1);
+      d3.csv("https://raw.githubusercontent.com/FranceMeli/progetto-minichallenges/master/static/gps.csv").then(function (datas) {
+        for (let i = 0; i < datas.length; i++) {
+          if (datas[i].id == 1 ){
+            dataset.push(datas[i]);
+          }
+        }
+        for (let i = 0; i < dataset.length; i=i+20) {
+          if(dataset[i].Timestamp.slice(0,10) == "01/06/2014"){
+              riprova.push(dataset[i])
+          }
+        }
+         A = d3.entries(riprova);
+        createMap(features, A);
+      });
+    });
+    function dateMatch(d) {
+      console.log(d)
+      var da = d.Timestamp
+      if (inputGiorno == da.slice(0,10)) {
+        this.parentElement.appendChild(this);
+        return "red";
+      } /*else {
+        return "#00FFFFFF";
+      }*/
+    }
+    function initialDate(d){
+      var da = d.Timestamp
+      if (da.slice(0,10) == "01/06/2014") {
+        this.parentElement.appendChild(this);
+        return "red";
+      } /*else {
+        return "#999";
+      }*/
 
-    });*/
-
-//Note that when you bind new data, you will be changing existing path elements
-//So you would also need to do a exit and modify existing paths
-
-
+    }
+    d3.select("#timeGiorni").on("input", function() {
+      update(+this.value);
+    });
+// update the fill of each SVG
+    function update(value) {
+      document.getElementById("rangeGiorni").innerHTML=Giorni[value];
+      inputGiorno = Giorni[value];
+      d3.selectAll(".move")
+          .attr("fill", dateMatch);
+    }
   }
 }</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 h3 {
   margin: 40px 0 0;
 }
@@ -79,6 +145,14 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  fill: #ccc;
+  stroke: #fff;
+  stroke-width: .5px;
+}
+
+path {
+  stroke-width: 1;
+  stroke: black;
+  opacity: .5;
 }
 </style>
